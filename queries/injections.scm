86f8ac3bb7240aba7f6c26210babe070bffc184b
George Harker <george@george-graphics.co.uk>
2026-04-07 14:34:08 -0700

    fix: split tagged-template and call-with-template injection patterns into separate rules

--- queries/injections.scm
+++ queries/injections.scm
@@ -5,20 +5,23 @@
 ((comment) @injection.content
   (#set! injection.language "comment"))
 
-; html(`...`), html`...`, sql(`...`), etc.
+; html`...`, sql`...`, etc. (tagged template literal form)
 (call_expression
   function: (identifier) @injection.language
-  arguments: [
-    (arguments
-      (template_string) @injection.content)
-    (template_string) @injection.content
-  ]
+  arguments: (template_string) @injection.content
   (#lua-match? @injection.language "^[a-zA-Z][a-zA-Z0-9]*$")
   (#offset! @injection.content 0 1 0 -1)
   (#set! injection.include-children)
-  ; Languages excluded from auto-injection due to special rules
-  ; - svg uses the html parser
-  ; - css uses the styled parser
+  (#not-any-of? @injection.language "svg" "css"))
+
+; html(`...`), sql(`...`), etc. (call-with-template-argument form)
+(call_expression
+  function: (identifier) @injection.language
+  arguments: (arguments
+    (template_string) @injection.content)
+  (#lua-match? @injection.language "^[a-zA-Z][a-zA-Z0-9]*$")
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.include-children)
   (#not-any-of? @injection.language "svg" "css"))
 
 ; svg`...` or svg(`...`)
